d3fe2937d8d80b1027321bff5315d2f542190c22
Azaki21421 <46106826+Azaki21421@users.noreply.github.com>
2025-03-23 01:10:31 +0300

    Share
    Someone create cryptography project and to lazy to change name (since, the first direction of the project was for usb)

---
+++ usbwork/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace usbwork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 191);
+            this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::usbwork.Properties.Resources.icons8_folders_64;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(262, 191);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 244);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 244);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 210);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 300);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DES crypt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ usbwork/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace usbwork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+        }
+
+        private static string PromptForPassword(string message)
+        {
+            string password = "";
+            Form prompt = new Form()
+            {
+                Width = 300,
+                Height = 150,
+                Text = "Enter password",
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            Label textLabel = new Label() { Left = 20, Top = 20, Text = message, AutoSize = true };
+            TextBox textBox = new TextBox() { Left = 20, Top = 50, Width = 240, UseSystemPasswordChar = true };
+            Button confirmation = new Button() { Text = "OK", Left = 90, Width = 100, Top = 80, DialogResult = DialogResult.OK };
+
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textLabel);
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.AcceptButton = confirmation;
+
+            if (prompt.ShowDialog() == DialogResult.OK)
+            {
+                password = textBox.Text;
+            }
+
+            return password;
+        }
+
+        private static byte[] GetTripleDESKey(string promptMessage)
+        {
+            string password = PromptForPassword(promptMessage);
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Encoding.UTF8.GetBytes(password)).Take(24).ToArray();
+            }
+        }
+
+        public static void EncryptFile(string inputFile, string outputFile)
+        {
+            byte[] key1 = GetTripleDESKey("Enter password 1:");
+            byte[] key2 = GetTripleDESKey("Enter password 2:");
+            byte[] key3 = GetTripleDESKey("Enter password 3:");
+
+            using (TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider())
+            {
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                byte[] data = File.ReadAllBytes(inputFile);
+
+                using (ICryptoTransform enc1 = tdes.CreateEncryptor(key1, null))
+                using (ICryptoTransform dec2 = tdes.CreateDecryptor(key2, null))
+                using (ICryptoTransform enc3 = tdes.CreateEncryptor(key3, null))
+                {
+                    byte[] step1 = enc1.TransformFinalBlock(data, 0, data.Length);
+                    byte[] step2 = dec2.TransformFinalBlock(step1, 0, step1.Length);
+                    byte[] finalData = enc3.TransformFinalBlock(step2, 0, step2.Length);
+                    File.WriteAllBytes(outputFile, finalData);
+                }
+            }
+        }
+
+        public static void DecryptFile(string inputFile, string outputFile)
+        {
+            byte[] key1 = GetTripleDESKey("Enter password 1:");
+            byte[] key2 = GetTripleDESKey("Enter password 2:");
+            byte[] key3 = GetTripleDESKey("Enter password 3:");
+
+            using (TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider())
+            {
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                byte[] data = File.ReadAllBytes(inputFile);
+
+                using (ICryptoTransform dec3 = tdes.CreateDecryptor(key3, null))
+                using (ICryptoTransform enc2 = tdes.CreateEncryptor(key2, null))
+                using (ICryptoTransform dec1 = tdes.CreateDecryptor(key1, null))
+                {
+                    byte[] step1 = dec3.TransformFinalBlock(data, 0, data.Length);
+                    byte[] step2 = enc2.TransformFinalBlock(step1, 0, step1.Length);
+                    byte[] finalData = dec1.TransformFinalBlock(step2, 0, step2.Length);
+                    File.WriteAllBytes(outputFile, finalData);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputFile = label1.Text;
+            string outputFile = inputFile + ".enc";
+            EncryptFile(inputFile, outputFile);
+            MessageBox.Show("The file is encrypted!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputFile = label1.Text;
+            string outputFile = inputFile.Replace(".enc", ".dec");
+            DecryptFile(inputFile, outputFile);
+            MessageBox.Show("The file is decrypted!");
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 0)
+                {
+                    string fullPath = files[0];
+                    string fileName = Path.GetFileName(fullPath);
+
+                    label1.Text = fileName;
+
+                    ToolTip toolTip = new ToolTip();
+                    toolTip.SetToolTip(label1, fullPath);
+
+                    pictureBox1.Visible = true;
+                }
+            }
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Move;
+        }
+    }
+}
